59ef50992d03adb8504dd7b16e2bdd7228de46f7
[nabernardo] <nabernardo93@gmail.com>
2017-07-26 23:21:30 -0700

    Add getting the date of the month

--- main.js
+++ main.js
@@ -1,9 +1,24 @@
-var url = 'http://quotes.stormconsultancy.co.uk/random.json';
-var quote = document.querySelector('p');
+// return a quote
+var dailyQuote = function getQuote() {
+  const url = 'http://quotes.stormconsultancy.co.uk/random.json';
+  const quote = document.querySelector('p');
 
-fetch(url)
-.then(response => response.json())
-.then(function(data) {
-  quote.textContent = data.quote;
-})
-.catch(error => console.log(error.message));
+  // get quote from api
+  fetch(url)
+  .then(response => response.json())
+  .then(function(data) {
+    quote.textContent = data.quote;
+  })
+  .catch(error => console.log(error.message));
+}
+
+dailyQuote();
+
+// get date
+var dayOfMonth = function getDate() {
+  var date = new Date();
+  var day = date.getDate();
+  return day;
+}
+
+dayOfMonth();
